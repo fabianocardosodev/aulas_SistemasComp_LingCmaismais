557b685e697c4791d5a4e48cbb4b91332745d8df
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-31 17:58:20 -0300

    aula C++ estruturas
    forma geral do conjunto de elementos
    dos programas em C++

---
+++ estruturas.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/* forma geral do conjunto de elementos
+dos programas 
+*/
+typedef float num_real;
+typedef int medida;
+typedef medida altura;
+
+altura alt= 20;
+int x = 4, i;
+
+int main (void){
+    i = alt/x;
+    return(0);
+}
+
